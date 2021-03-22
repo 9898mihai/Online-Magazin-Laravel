514099b85dd21571d8de0a1c1cc9ee5591640be0
mihai9898 <41786680+mihai9898@users.noreply.github.com>
2021-03-22 11:59:51 +0200

    responsive slider 2.0

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,26 +2,18 @@
 @section('title', 'Pagina principala')
 @section('content')
 <div class="container">
-
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-        <!-- Wrapper for slides -->
+    <div id="myCarousel" class="carousel slide" >
         <div class="carousel-inner">
             @foreach ($banners as $banner)
             <div class="item @if($banner->id === $banner->first()->id) {{ 'active' }} @endif">
-                <img src="{{Storage::url($banner->image)}}" alt="">
+                <div class="image-size" style="background-image: url('{{ Storage::url($banner->image)}}');">
+                </div>
                 <div class="carousel-caption">
                     <h3>{{$banner->name}}</h3>
                     <p>{{$banner->description}}</p>
                 </div>
             </div>
         @endforeach
-        <!-- Left and right controls -->
         <a class="left carousel-control" href="#myCarousel" data-slide="prev">
             <span class="glyphicon glyphicon-chevron-left"></span>
             <span class="sr-only">Previous</span>
