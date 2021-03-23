858245ff34d080f9a091af4646f0311975baf17d
mihai9898 <41786680+mihai9898@users.noreply.github.com>
2021-03-23 12:25:05 +0200

    filter price for category

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,6 +6,7 @@ use App\Models\Banner;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MainController extends Controller
 {
@@ -14,7 +15,7 @@ class MainController extends Controller
         $banners = Banner::get();
         $productsQuery = Product::query();
 
-        if ($request->filled('price_from')) {
+       if ($request->filled('price_from')) {
             $productsQuery->where('price','>=', $request->price_from);
         }
 
@@ -31,15 +32,37 @@ class MainController extends Controller
         return view('categories', compact('categories'));
     }*/
 
-    public function category($code) {
-        $category = Category::where('code', $code)->first();
+    public function category(Request $request, $code) {
         $categories = Category::get();
+        $category_id = Category::where('code', $code)->first();
+        $category = DB::table('categories');
+        $category ->join('products', 'category_id', '=', 'categories.id');
+//        $category ->join('products', 'category_id', '=', $category_id->id);
+
+        if($request->filled('price_from')) {
+            $category->where('price','>=', $request->price_from);
+        }
+
+        $cat = $category->get();
+        
+       /* $productsQuery = Product::query();
+
+        if ($request->filled('price_from')) {
+            $productsQuery->where('price','>=', $request->price_from);
+        }
+
+        if ($request->filled('price_to')) {
+            $productsQuery->where('price','<=', $request->price_to);
+        }
+
+        $category = $productsQuery->paginate(10);*/
         return view('category', compact('category','categories'));
     }
 
     public function product($category,$product) {
         $product = Product::where('code', $product)->first();
         $categories = Category::get();
+
         return view('product', compact('product','categories'));
     }
 
--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -9,6 +9,22 @@
             {{$category -> name}}
         </h1>
         <div class="row">
+            <div class="starter-template">
+                <form method="GET" action="{{ route('category',[$category->code]) }}">
+                    <div class="filters row">
+                        <div class="col-md-auto">
+                            Price
+                            <label for="price_from">min:<input type="text" name="price_from" id="price_from" size="6" value="{{request()->price_from}}">
+                            </label>
+                            <label for="price_to">max:<input type="text" name="price_to" id="price_to" size="6" value="{{request()->price_to}}">
+                            </label>
+                        </div>
+                        <div class="col-md-auto">
+                            <a href="{{ route('category',[$category->code]) }}" class="btn btn-warning">Reset</a>
+                            <button type="submit" class="btn btn-primary">Filter</button>
+                        </div>
+                    </div>
+                </form>
             @foreach ($category->products as $product)
                 @include('card',compact('product'))
             @endforeach
